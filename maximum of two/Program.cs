5fd1cda291b4d65b640376c08d71dd9c4acab93e
GalinaS <peraskevasokolova@gmail.com>
2022-08-22 19:37:31 +0400

    максимальное из двух чисел

---
+++ maximum of two/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число:");
+string input1 = Console.ReadLine();
+double a = double.Parse(input1);
+Console.WriteLine("Введите второе число:");
+string input2 = Console.ReadLine();
+double b = double.Parse(input2);
+if (a>b)
+{
+    Console.Write("max=");
+    Console.WriteLine(a);
+}    
+else
+{
+   Console.Write("max=");
+    Console.WriteLine(b); 
+}    
+
