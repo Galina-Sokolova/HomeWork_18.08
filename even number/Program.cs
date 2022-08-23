679973a57c63df4f0f86e44ea7dc0d86140cc016
GalinaS <peraskevasokolova@gmail.com>
2022-08-23 13:47:22 +0400

    определение является ли число чётным

---
+++ even number/Program.cs
@@ -0,0 +1,13 @@
+﻿//программа принимает на вход число и выдаёт, является ли оно чётным
+Console.WriteLine("Введите число:");
+int input = Convert.ToInt32(Console.ReadLine());
+
+int rem = input % 2;
+if (rem == 0)
+{
+    Console.WriteLine("число "+input+" является чётным");
+}
+else
+{
+    Console.WriteLine("число"+input+"является нечётным");
+}
