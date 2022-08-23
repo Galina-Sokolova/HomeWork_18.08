1dd8e1f55d7d7f729a0d5a22eb77ca7f104542ca
GalinaS <peraskevasokolova@gmail.com>
2022-08-23 14:51:04 +0400

    вывод последовательности четных чисел

---
+++ sequence of even numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿// программа выводит последовательность четных чисел от 1 до введенного числа
+Console.Write("Введите целое число:");
+int input = Convert.ToInt32(Console.ReadLine());
+int first = 1;
+//если введено положительное число
+if (input > 0)
+{
+    while (first < input+1)
+{
+    if (first%2 == 0)
+    {
+        Console.WriteLine(first);
+    }
+    first++;
+}
+}
+//если введен 0 или отрицательное число
+else
+{
+    while (input < first)
+{
+    if (input%2 == 0)
+    {
+        Console.WriteLine(input);
+    }
+    input++;
+}
+}
+
