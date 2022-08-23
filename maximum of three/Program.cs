9b8be3fb22ff1db6e53b1ef71c07ef00698906f5
GalinaS <peraskevasokolova@gmail.com>
2022-08-23 13:25:01 +0400

    нахождение максимального сисла из трех

---
+++ maximum of three/Program.cs
@@ -0,0 +1,20 @@
+﻿// программa принимает на вход три числа и выдаёт максимальное из них
+Console.WriteLine("Введите первое число:");
+double input1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+double input2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите третье число:");
+double input3 = Convert.ToDouble(Console.ReadLine());
+
+double max = input1;
+
+if (input2 > max) max = input2;
+if (input3 > max) max = input3;
+
+Console.Write("максимальное число равно ");
+Console.WriteLine(max);
+   
+    
+
